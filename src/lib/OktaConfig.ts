21bf7ed8a66ea74214db6310252a2f6f3143050d
Christos Rentzos <rentzosc@gmail.com>
2024-08-05 19:06:53 +0300

    login works with https

--- src/lib/OktaConfig.ts
+++ src/lib/OktaConfig.ts
@@ -1,8 +1,8 @@
 export const oktaConfig = {
-    clientId: "0oaij1xuwoEBiGjAp5d7",
-    issuer: "https://dev-99675739.okta.com/oauth2/default",
-    redirectUri: "http://localhost:3000/login/callback",
-    scopes: ["openid", "profile", "email"],
-    pkce: true,
-    disableHttpsCheck: true,
-}
+  clientId: "0oaij1xuwoEBiGjAp5d7",
+  issuer: "https://dev-99675739.okta.com/oauth2/default",
+  redirectUri: "https://localhost:3000/login/callback",
+  scopes: ["openid", "profile", "email"],
+  pkce: true,
+  disableHttpsCheck: true,
+};
