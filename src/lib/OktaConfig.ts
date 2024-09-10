86563e9302b0bd9e75f77cffc17d9ee1f2783d7b
Christos Rentzos <rentzosc@gmail.com>
2024-09-10 16:39:40 +0300

    modified oktaconfiguration

--- src/lib/OktaConfig.ts
+++ src/lib/OktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: "0oaij1xuwoEBiGjAp5d7",
   issuer: "https://dev-99675739.okta.com/oauth2/default",
-  redirectUri: "https://localhost:3000/login/callback",
+  redirectUri: "https://my-react-library-app.netlify.app/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
   disableHttpsCheck: true,
